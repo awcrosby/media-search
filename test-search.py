2fbef523e7d91b9bbe5578a26052721c993a5a44
Andrew Crosby <awcrosby@gmail.com>
2016-12-14 07:29:09 +0000

    added netflixroulette api call

--- test-search.py
+++ test-search.py
@@ -1,44 +1,39 @@
 #!/usr/bin/env python
 
-import requests
-#import requests_cache
 import json
+import requests
+from NetflixRoulette import *
+#import requests_cache
+#todo: add flask front end
 
 #def main():
 
-temp_apikey = 'yh7JUQD5rcNN9VVzjp6ovcTna1581H'
+print "Is movie on Netlix, AMX, Hulu, or Hbo streaming...?"
+moviequery = raw_input('Movie search: ') #gb wants triple encoded
+print "Web stream sources (if any):"
 
-##enter movie query
-moviequery =  'clueless' #need to triple encode this
+##NetflixRoulette movie search
+try:
+   get_netflix_id(moviequery)
+   print "netflix"
+except Exception:
+   pass #sys.exc_clear()
 
-##perform movie search
-apikey = temp_apikey
-region = 'US'
-baseapiurl = 'http://api-public.guidebox.com/v1.43/%s/%s' % (region, apikey)
-moviesearchurl = '%s/search/movie/title/%s/exact' % (baseapiurl, moviequery)
-#r = requests.get(moviesearchurl)
-#moviesearch = json.loads(r.text)
+# ##guidebox movie search
+# apikey = 'yh7JUQD5rcNN9VVzjp6ovcTna1581H' #temp dev key
+# #go here for new dev key: https://api.guidebox.com/production-key
+# region = 'US'
+# baseapiurl = 'http://api-public.guidebox.com/v1.43/%s/%s' % (region, apikey)
+# moviesearchurl = '%s/search/movie/title/%s/exact' % (baseapiurl, moviequery)
+# r = requests.get(moviesearchurl)
+# moviesearch = json.loads(r.text)
+# id = moviesearch["results"][0]["id"]
 
-#print "title: ", moviesearch["results"][0]["title"]
-#print "id: ", moviesearch["results"][0]["id"]
-id = 42929
-
-##get movie info
-movieinfourl = '%s/movie/%s' % (baseapiurl, id)
-print movieinfourl
-r = requests.get(movieinfourl)
-movieinfo = json.loads(r.text)
-#print "subscription_web_sources: ", movieinfo["subscription_web_sources"]
+# ##guidebox get movie info
+# movieinfourl = '%s/movie/%s' % (baseapiurl, id)
+# r = requests.get(movieinfourl)
+# movieinfo = json.loads(r.text)
  
-##print stream links and show as yes/no
-for websource in movieinfo["subscription_web_sources"]:
-   #if websource[0]["source"] = "hulu_plus": print "Hulu Plus supported"
-   #if websource[0]["source"] = "hbo_now": print "HBO supported"
-   print websource["source"]
-   
-##todo check all sub sources in py and print them out to see which ones to look for or ignore... like amazon prime ones that are not pure prime: https://api.guidebox.com/apidocs
-##todo find way to ignore non sub things, and print out all you find
-##todo - maybe add caching, add to flask so can enter form
-
-
-
+# ##guidebox print avail web stream sources
+# for websource in movieinfo["subscription_web_sources"]:
+   # print websource["source"]
