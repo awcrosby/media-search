c0c00099d08ed7d1ee23ec1683b190954e7efb34
Andrew Crosby <awcrosby@gmail.com>
2016-12-14 07:31:33 +0000

    added netflixroulette api call

--- test-search.py
+++ test-search.py
@@ -19,21 +19,21 @@ try:
 except Exception:
    pass #sys.exc_clear()
 
-# ##guidebox movie search
-# apikey = 'yh7JUQD5rcNN9VVzjp6ovcTna1581H' #temp dev key
-# #go here for new dev key: https://api.guidebox.com/production-key
-# region = 'US'
-# baseapiurl = 'http://api-public.guidebox.com/v1.43/%s/%s' % (region, apikey)
-# moviesearchurl = '%s/search/movie/title/%s/exact' % (baseapiurl, moviequery)
-# r = requests.get(moviesearchurl)
-# moviesearch = json.loads(r.text)
-# id = moviesearch["results"][0]["id"]
+##guidebox movie search
+apikey = 'yh7JUQD5rcNN9VVzjp6ovcTna1581H' #temp dev key
+#go here for new dev key: https://api.guidebox.com/production-key
+region = 'US'
+baseapiurl = 'http://api-public.guidebox.com/v1.43/%s/%s' % (region, apikey)
+moviesearchurl = '%s/search/movie/title/%s/exact' % (baseapiurl, moviequery)
+r = requests.get(moviesearchurl)
+moviesearch = json.loads(r.text)
+id = moviesearch["results"][0]["id"]
 
-# ##guidebox get movie info
-# movieinfourl = '%s/movie/%s' % (baseapiurl, id)
-# r = requests.get(movieinfourl)
-# movieinfo = json.loads(r.text)
+##guidebox get movie info
+movieinfourl = '%s/movie/%s' % (baseapiurl, id)
+r = requests.get(movieinfourl)
+movieinfo = json.loads(r.text)
  
-# ##guidebox print avail web stream sources
-# for websource in movieinfo["subscription_web_sources"]:
-   # print websource["source"]
+##guidebox print avail web stream sources
+for websource in movieinfo["subscription_web_sources"]:
+   print websource["source"]
