77458b65b2d8fc09f00ebeeeacaaa367b492a79d
Andrew Crosby <awcrosby@gmail.com>
2017-03-24 03:55:52 +0000

    moved from guidbox api v1.x to v2

--- flaskapp.py
+++ flaskapp.py
@@ -7,14 +7,18 @@ results = []
 
 @app.route('/')
 def home():
-    return render_template('index.html', display="")
+    return render_template('index.html')
 
 @app.route('/result', methods = ['POST'])
 def displayresults():
-    movieq = request.form['movieq']
-    result = mediasearch(movieq)
-    display = "You can stream on: %s" % result
-    return render_template('index.html', display=display)
+    query = request.form['movieq']
+    qtype = 'movie'
+    sources = mediasearch(query, qtype)
+    if sources:
+        display = 'The %s %s is available on these streaming services' % (qtype, query)
+    else:
+        display = 'The %s %s doesn\'t seem available on these streaming services' % (qtype, query)
+    return render_template('index.html', display=display, sources=sources)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8181)
--- mediasearch.py
+++ mediasearch.py
@@ -2,37 +2,33 @@
 import json
 import requests
 from NetflixRoulette import *
+import guidebox
 
-def mediasearch( moviequery ):
-    results = ""
-    ##NetflixRoulette movie search
-    try:
-        get_netflix_id(moviequery)
-        results += "netflix"
+def mediasearch(query, qtype):
+    sources = []
+    try:  #NetflixRoulette search
+        get_netflix_id(query)  #returns error if cannot find it netflix's library
+        sources.append('Netflix')
     except Exception:
         pass #sys.exc_clear()
 
-    ##guidebox movie search
-    gbkey = json.loads(open('apikeys.json').read())["guidebox_prod_apikey"]
-    region = 'US'
-    baseapiurl = 'http://api-public.guidebox.com/v1.43/%s/%s' % (region, gbkey)
-    moviesearchurl = '%s/search/movie/title/%s/exact' % (baseapiurl, moviequery) #gb wants triple encoded
-    r = requests.get(moviesearchurl)
-    moviesearch = json.loads(r.text)
-    try:
-        id = moviesearch["results"][0]["id"] #fix traceback on q=darko
-        ##guidebox get movie info
-        movieurl = '%s/movie/%s' % (baseapiurl, id)
-        r = requests.get(movieurl)
-        movieinfo = json.loads(r.text)
-        
-        ##guidebox avail stream sources
-        for websource in movieinfo["subscription_web_sources"]:
-            results += websource["source"]
+    try:  #guidebox apiv2 search
+        guidebox.api_key = json.loads(open('apikeys.json').read())["guidebox_prod_apikey"]
+        if (qtype == 'movie'):
+            movies = guidebox.Search.movies(field='title', query=query)
+            gbid = movies['results'][0]['id']
+            media = guidebox.Movie.retrieve(id=gbid)  #dive deeper into api to find sources
+        elif (qtype == 'show'):
+            media = 'temp'
+        for websource in media['subscription_web_sources']:
+            sources.append(websource['source'])
     except Exception:
-        pass #sys.exc_clear()
+        pass
+    f = open( 'movies.txt', 'w' )
+    f.write( 'dict = ' + repr(movies) + '\n' )
+    f.close()
     
-    return results
+    return sources
 
 if __name__ == "__main__":
     print "Check if movie is on Netlix, AMZ, Hulu, or HBO."
