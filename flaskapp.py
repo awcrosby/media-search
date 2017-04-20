57f3c66745238ac2618a7b89e32c8a28d7933130
Andrew Crosby <awcrosby@gmail.com>
2017-04-20 01:17:18 +0000

    better display of seasons

--- flaskapp.py
+++ flaskapp.py
@@ -14,8 +14,9 @@ def home():
 @app.route('/search', methods=['GET'])
 def search():
     query = request.args.get('q')  # or if POST: query = request.form['q']
-    qtype = 'movie'  # set default value if not included
     qtype = request.args.get('type')
+    if (qtype != 'movie') & (qtype != 'show'):
+        qtype = 'movie'  # set default value if not included
     if not query:  # exit early if query is blank
         return render_template('index.html')
     guidebox.api_key = json.loads(open('apikeys.json').read())["guidebox"]
@@ -28,55 +29,37 @@ def search():
         gbid = medias['results'][0]['id']  # take first result
         media = guidebox.Movie.retrieve(id=gbid)  # more info on movie
 
-        for websource in media['subscription_web_sources']:
-            x = {'name': websource['source'], 'link': websource['link']}
-            sources[websource['source']] = x
-        med = {'title': media['title'], 'year': media['release_year'],
-               'imdb': media['imdb']}
+        for ws in media['subscription_web_sources']:
+            x = {'name': ws['source'], 'link': ws['link']}
+            sources[ws['source']] = x
+        med = {'title': media['title'],  # create to send to template
+               'year': media['release_year'],
+               'imdb': media['imdb'],
+               'img': media['poster_120x171']}
     elif qtype == 'show':
         medias = guidebox.Search.shows(precision='fuzzy', field='title', query=query)
         if not (medias['total_results'] > 0):  # exit early if no search results
             return render_template('index.html', isresult=0, query=query, qtype=qtype)
         gbid = medias['results'][0]['id']  # take first result
-        media = guidebox.Show.episodes(id=gbid, include_links=True, limit=200)
-        
-        seasons = []
+        media = guidebox.Show.episodes(id=gbid, include_links=True, limit=250)
+
         for x in media['results']:  # go thru every episode and add source
-            #for websource in x['subscription_web_sources']:
-            #    y = {'name': websource['source'], 'link': websource['link']}
-            #    sources[websource['source']] = y
-            #if x['season_number'] not in seasons:
-            #    seasons.append(x['season_number'])
-            
-            for websource in x['subscription_web_sources']:
-                if websource['source'] not in sources: # make new source entry
-                    y = {'name': websource['source'],
-                         'link': websource['link'],
+            for ws in x['subscription_web_sources']:
+                if ws['source'] not in sources:  # make new source entry
+                    y = {'name': ws['source'],
+                         'link': ws['link'],
                          'epcount': 1,
-                         'seasons': set(str(x['season_number']))}
-                    sources[websource['source']] = y
+                         'seasons': list(str(x['season_number']))}
+                    sources[ws['source']] = y
                 else:  # increase epcount and ensure season is accounted for
-                    sources[websource['source']]['epcount'] += 1
-                    sources[websource['source']]['seasons'].add(str(x['season_number']))
-                    
-
-        print "resultcount = ", len(media['results'])
-        for x in sources:
-            print "source = " + sources[x]['name']
-            print "epcount = ", sources[x]['epcount']
-            print "seasons = ", sources[x]['seasons']
-        #source: name, link, ep, season[]
-        #sources['netflix'] = name:netflix, link:..., ep:#, season:[] 
-
-        #media = guidebox.Show.available_content(id=gbid)  # non-episode method
-        #for allsource in media['results']['web']['episodes']['all_sources']:
-        #    if allsource['type'] == 'subscription':
-        #        x = {'name': allsource['source'], 'link': 'none'}
-        #        sources.append(x)
+                    sources[ws['source']]['epcount'] += 1
+                    if str(x['season_number']) not in sources[ws['source']]['seasons']:
+                        sources[ws['source']]['seasons'].append(str(x['season_number']))
+                sources[ws['source']]['seasons'].sort()
 
         m = medias['results'][0]
         med = {'title': m['title'], 'year': m['first_aired'][:4],
-               'imdb': m['imdb_id']}
+               'imdb': m['imdb_id'], 'img': m['artwork_208x117']}
 
     # alter display name of sources and limit what is displayed
     mapping = {'netflix': 'Netflix',
@@ -84,19 +67,19 @@ def search():
                'hulu_plus': 'Hulu',
                'amazon_prime': 'Amazon Prime',
                'showtime_subscription': 'Showtime (and Amazon/Hulu + Showtime)'}
-    for s in sources:
-        if sources[s]['name'] in mapping.keys():
-            sources[s]['name'] = mapping[sources[s]['name']]
+    for k in sources.keys():
+        if k in mapping.keys():
+            sources[k]['name'] = mapping[k]
         else:
-            # sources.remove(source)
-            sources[s]['name'] = '~' + sources[s]['name']
+            del sources[k]
 
     # build other results to send to template
     other_results = []
     for m in medias['results'][1:]:
-        x = {'href': 'search?mq=' + str(m['title']), 'title': str(m['title'])}
-        if(m['wikipedia_id'] != 0):  # filters out the very obscure
-            other_results.append(x)
+        x = {'href': 'search?q=' + m['title'] + '&type=' + qtype,
+             'title': m['title']}
+        if (m['wikipedia_id'] != 0) & (m['wikipedia_id'] is not None):
+            other_results.append(x)  # filters out the very obscure
 
     # temp debugging
     f = open('medias.txt', 'w')
