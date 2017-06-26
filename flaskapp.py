1057a21fe8f2b13ff6d411638d01f7546fea8a36
Andrew Crosby <awcrosby@gmail.com>
2017-06-26 16:23:19 +0000

    added flashmessaging, navbar, registration page/db writing. extracted searchbar

--- flaskapp.py
+++ flaskapp.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash, session, logging
 import json
 import guidebox
 import time
@@ -7,6 +7,9 @@ import urllib
 import pymongo
 import pprint
 import logging
+import datetime
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
 from shared_func import get_media, get_show_ep, get_all_ep, add_src_display
 app = Flask(__name__)
 
@@ -25,6 +28,43 @@ logging.basicConfig(filename='/home/awcrosby/media-search/'
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        client = pymongo.MongoClient('localhost', 27017)
+        db = client.MediaData
+        try: 
+            db.Users.insert_one(
+            {
+                'name': name,
+                'email': email,
+                'password': password,
+                'dateCreated': datetime.datetime.utcnow()
+            })
+        except pymongo.errors.DuplicateKeyError, e:
+            flash('That email is already registered', 'danger')
+            return redirect(url_for('register'))
+
+        flash('You are now registered and can log in', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', form=form)
 
 
 @app.route('/edit_watchlist', methods=['GET', 'POST'])
@@ -148,4 +188,5 @@ def search(mtype='movie', gbid=None, query=''):
                            mtype=mtype, other_results=other_results)
 
 if __name__ == "__main__":
+    app.secret_key='3d6gtrje6d2rffe2jqkv'
     app.run(debug=True, host='0.0.0.0', port=8181)
