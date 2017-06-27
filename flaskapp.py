06477ec3302499b16450f1ab44172168da547b55
Andrew Crosby <awcrosby@gmail.com>
2017-06-27 14:33:11 +0000

    added user login and session to display watchlist for particular user

--- flaskapp.py
+++ flaskapp.py
@@ -12,6 +12,7 @@ from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 from shared_func import get_media, get_show_ep, get_all_ep, add_src_display
 app = Flask(__name__)
+from functools import wraps
 
 '''webframework flaskapp high-level functionality:
     resolve user search query to a specific media
@@ -24,7 +25,7 @@ logging.basicConfig(filename='/home/awcrosby/media-search/'
                     format='%(asctime)s %(levelname)s: %(message)s',
                     level=logging.INFO)
 
-
+# landing page
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -40,14 +41,17 @@ class RegisterForm(Form):
     confirm = PasswordField('Confirm Password')
 
 
+# user registration
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
+        # get form fields
         name = form.name.data
         email = form.email.data
         password = sha256_crypt.encrypt(str(form.password.data))
 
+        # connect to db and insert new user
         client = pymongo.MongoClient('localhost', 27017)
         db = client.MediaData
         try: 
@@ -56,52 +60,118 @@ def register():
                 'name': name,
                 'email': email,
                 'password': password,
-                'dateCreated': datetime.datetime.utcnow()
+                'dateCreated': datetime.datetime.utcnow(),
+                'movieq': [],
+                'showq': []
             })
         except pymongo.errors.DuplicateKeyError, e:
             flash('That email is already registered', 'danger')
             return redirect(url_for('register'))
 
         flash('You are now registered and can log in', 'success')
-        return redirect(url_for('home'))
+        return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
 
+# user login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # get form fields
+        email = request.form['email']
+        password_candidate = request.form['password']
+
+        # connect to db
+        client = pymongo.MongoClient('localhost', 27017)
+        db = client.MediaData
+
+        # get user by email and check password
+        user = db.Users.find_one( {'email': email} )
+        if user:
+            if sha256_crypt.verify(password_candidate, user['password']):
+                # passwords match
+                session['logged_in'] = True
+                session['email'] = email
+                flash('You are now logged in', 'success')
+                return redirect(url_for('watchlist'))
+            else:
+                return render_template('login.html', error='Invalid login')
+        else:
+            return render_template('login.html', error='Email not found')
+    return render_template('login.html')
+
+
+# check if users logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, please login', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+
+# user logout
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.clear()
+    flash('You are now logged out', 'success')
+    return redirect(url_for('login'))
+
+
+# add or delete item from watchlist is processed here, no UI
 @app.route('/edit_watchlist', methods=['GET', 'POST'])
+@is_logged_in
 def edit_watchlist():
     if request.method == 'POST':
         # connect to db, get user, get form data
         client = pymongo.MongoClient('localhost', 27017)
         db = client.MediaData
-        email = 'awcrosby@gmail.com'  # in future get this from session
+        email = session['email']
         operation = request.form['operation']
         mtype = request.form['mtype']
         gbid = int(request.form['gbid'])
 
         # update the queue for movie or show
         if operation == 'add' and mtype == 'movie':
-            db.Users.find_one_and_update({'email': email},
-                                         {'$push': {'movieq': gbid}})
+            user = db.Users.find_one({'email': email})
+            if gbid not in user['movieq']:
+                db.Users.find_one_and_update({'email': email},
+                                             {'$push': {'movieq': gbid}})
+                flash('Movie added to watchlist', 'success')
+            else:
+                flash('Movie already in watchlist', 'info')
         elif operation == 'add' and mtype == 'show':
-            db.Users.find_one_and_update({'email': email},
-                                         {'$push': {'showq': gbid}})
+            user = db.Users.find_one({'email': email})
+            if gbid not in user['showq']:
+                db.Users.find_one_and_update({'email': email},
+                                             {'$push': {'showq': gbid}})
+                flash('Show added to watchlist', 'success')
+            else:
+                flash('Show already in watchlist', 'info')
         elif operation == 'delete' and mtype == 'movie':
             db.Users.find_one_and_update({'email': email},
                                          {'$pull': {'movieq': gbid}})
+            flash('Movie deleted from watchlist', 'success')
         elif operation == 'delete' and mtype == 'show':
             db.Users.find_one_and_update({'email': email},
                                          {'$pull': {'showq': gbid}})
+            flash('Show deleted from watchlist', 'success')
         client.close()
     return redirect(url_for('watchlist'))
 
 
+# display user's watchlist
 @app.route('/watchlist')
+@is_logged_in
 def watchlist():
     # connect to db and get user
     start = time.time()
     client = pymongo.MongoClient('localhost', 27017)
     db = client.MediaData
-    email = 'awcrosby@gmail.com'  # in future get this from session
+    email = session['email']
     user = db.Users.find_one({'email': email})
 
     # get user watchlist ids
@@ -121,8 +191,11 @@ def watchlist():
 
     client.close()
     print 'time to get media of full watchlist: ', time.time() - start
+    print 'session.email=', email
     return render_template('watchlist.html', medias=watchlist)
 
+
+# process media search or id lookup and display
 @app.route('/<mtype>/id/<int:gbid>')
 @app.route('/search', methods=['GET'])
 def search(mtype='movie', gbid=None, query=''):
@@ -186,6 +259,7 @@ def search(mtype='movie', gbid=None, query=''):
 
     return render_template('index.html', media=media, query=query,
                            mtype=mtype, other_results=other_results)
+
 
 if __name__ == "__main__":
     app.secret_key='3d6gtrje6d2rffe2jqkv'
